1b9bd5643d33ad3b358fd1c8afe825b07018eb3d
Bashtovaya, Marina (mb139h) <mb139h@us.att.com>
2019-05-26 21:31:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Project: Log Analysis
+
+First is a first project written as a part of then Full Stack Web Developer Nanodegree.
+
+# Pre-requisites
+
+Database: psql
+Language: python 2.7
+
+1. data provided by the course (newsdata.sql)
+2. views below
+
+# view calculates the usage of each log item whose path contains something valuable
+# it will return the usage count and will covert the path to the more sutable for article search string 
+create view article_usage as
+	select REPLACE(REPLACE(path,'-',' '),'/article/','') as starts_with, COUNT(*) usage
+	from log
+	where path != '/'
+	group by path;
+
+# view calculates total number of logged requests and number of errors per day
+create view daily_access as
+	select DATE(time) as logdate, SUM(CASE WHEN status != '200 OK' THEN 1 ELSE 0 END) as errcount, COUNT(*) as total
+	from daily_access
+	from log
+	group by logdate;	
+
+# How to run
+
+python analyzer.py
+
+# Expected Result
+
+The program will use the provided data set to calculate answers to the 3 questions:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors? 
+
+---------------------------------
+FOR EXAMPLE:
+---------------------------------
+vagrant@vagrant:/vagrant/project1$ python analyzer.py
+
+Top popular articles:
+1. Candidate is jerk, alleges rival - 338647 views
+2. Bears love berries, alleges bear - 253801 views
+3. Bad things gone, say good people - 170098 views
+
+Author popularity:
+1. Rudolf von Treppenwitz - 423457 views
+2. Ursula La Multa - 338184 views
+3. Anonymous Contributor - 170098 views
+4. Markoff Chaney - 84557 views
+
+Daily errors over 1 percent:
+July      17, 2016 -    2.3% errors
---
+++ analyzer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+#
+# Log Analyzer
+
+import psycopg2
+
+# What are the most popular three articles of all time
+query_top_popular_articles = "\
+    select a.title, u.usage \
+    from articles a, \
+        (select starts_with, usage \
+            from article_usage \
+            order by usage desc \
+            fetch first {0} rows only) u \
+    where LOWER(a.title) like CONCAT(u.starts_with,'%') \
+    order by u.usage desc;"
+
+# Who are the most popular article authors of all time
+query_author_popularity = "\
+    select n.name, SUM(u.usage) as total \
+    from authors n, \
+        articles a, \
+        (select starts_with, usage from article_usage) u \
+    where n.id = a.author and LOWER(a.title) like CONCAT(u.starts_with,'%') \
+    group by n.name \
+    order by total desc;"
+
+# On which days did more than X% of requests lead to errors
+query_daily_errors = "\
+    select TO_CHAR(logdate, 'Month DD, YYYY'), \
+           TO_CHAR(errcount*100.0/total, '999.9') \
+    from daily_access \
+    where errcount*100.0/total > {0};"
+
+
+def fetch_db_result(query_to_run):
+    try:
+        db = psycopg2.connect("dbname=news")
+        c = db.cursor()
+        c.execute(query_to_run)
+        return c.fetchall()
+    finally:
+        if c:
+            c.close()
+        if db:
+            db.close()
+
+
+def print_top_popular_articles(top_n):
+    print '\nTop popular articles:'
+    result = fetch_db_result(query_top_popular_articles.format(top_n))
+    for index in range(len(result)):
+        article = result[index][0]
+        count = str(result[index][1])
+        print str(index+1)+'. '+article+' - '+count+' views'
+
+
+def print_author_popularity():
+    print '\nAuthor popularity:'
+    result = fetch_db_result(query_author_popularity)
+    for index in range(len(result)):
+        author = result[index][0]
+        count = str(result[index][1])
+        print str(index+1)+'. '+author+' - '+count+' views'
+
+
+def print_daily_errors(min_percent):
+    print '\nDaily errors over '+str(min_percent)+' percent:'
+    result = fetch_db_result(query_daily_errors.format(min_percent))
+    for index in range(len(result)):
+        print result[index-1][0]+' - '+str(result[index-1][1])+'% errors'
+
+
+if __name__ == '__main__':
+    print_top_popular_articles(3)
+    print_author_popularity()
+    print_daily_errors(1)
+    print '\n'
